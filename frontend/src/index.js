253aa464514e4fc43cb2e9516be9b960102c9ad0
Alegriaa <brpereir@ucsc.edu>
2021-04-11 00:24:42 -0700

    login page implemented

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,9 @@ import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Register from "./components/Register";
+import Login from "./components/Login";
+// import Logout from "./components/Logout";
+
 import App from "./App";
 
 const routing = (
@@ -13,6 +16,8 @@ const routing = (
     <Switch>
       <Route exact path="/" component={App} />
       <Route path="/register" component={Register} />
+      <Route path="/login" component={Login} />
+      {/* <Route path="/logout" component={Logout} /> */}
     </Switch>
     <Footer />
   </Router>
